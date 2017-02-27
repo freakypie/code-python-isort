bba613cd654f7a5a278d9ae003fff3c8203f011e
John Leith <jleith@velocitywebworks.com>
2017-02-27 16:31:41 -0700

    no messages unless there is an error

--- extension.js
+++ extension.js
@@ -8,10 +8,6 @@ const vscode = require('vscode');
 // your extension is activated the very first time the command is executed
 function activate(context) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "code-python-isort" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
@@ -20,10 +16,12 @@ function activate(context) {
 
         var filename = vscode.window.activeTextEditor.document.fileName;
         var process = spawn('isort', [filename]);
-        var output = process.stdout.toString() || "isorted " + filename;
+        var output = process.stdout.toString();
 
         // Display a message box to the user
-        vscode.window.showInformationMessage(output);
+        if (output) {
+            vscode.window.showInformationMessage(output);
+        }
     });
 
     context.subscriptions.push(disposable);
